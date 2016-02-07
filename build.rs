f35536887be945310b01a89fa343a5b43e11ed14
Tae-il Lim <trlim@iotok.kr>
2016-02-07 13:23:27 +0900

    Fix library search path

--- build.rs
+++ build.rs
@@ -3,8 +3,10 @@ extern crate cmake;
 use cmake::Config;
 
 fn main() {
-    let dst = Config::new("usrsctp")
+    let mut dst = Config::new("usrsctp")
                     .build();
+
+    dst.push("lib");
 
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=usrsctp");
